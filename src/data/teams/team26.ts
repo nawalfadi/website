15af3cf13bfcc7d207facca1f2f880b753682c00
nawal <nawalfadi@outlook.com>
2026-02-18 06:17:25 +0300

    Update

--- src/data/teams/team26.ts
+++ src/data/teams/team26.ts
@@ -47,6 +47,7 @@ export const engineering26: TeamMember[] = [
 		nameKey: '26name5',
 		positionKey: '26name5',
 		team: ['Engineering'],
+		image: '/images/humans/Mohamed Sawan.png',
 		social: {
 			github: 'https://github.com/mohamedsawann',
 			linkedin: 'https://www.linkedin.com/in/mohammed-sawan-56ba6b251/'
